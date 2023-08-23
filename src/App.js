059b9b3f32afb6cf730d0a2059fa38160761059e
mdshowkatislam <mdshowkatislam8876643@yahoo.com>
2023-08-24 02:49:44 +0600

    Showing the fetched data on UI and Project is completed

--- src/App.js
+++ src/App.js
@@ -14,14 +14,11 @@ function App() {
   });
 
   return (
-    <div className="App  flex justify-center">
-      <div
-        className="border-indigo-500/100 border-4 bottom-4 rounded-3xl p-2 mt-20 text-2xl bg-slate-400  w-2/5 
-          flex-col 
-      "
-      >
+    <div className="flex justify-center App">
+      <div className="flex-col w-full p-2 mt-20 text-2xl border-4 border-indigo-500/100 bottom-4 rounded-3xl bg-slate-400 ">
         <QueryClientProvider client={client}>
-          <Suspense fallback={<h1>Data Loading.......</h1>}>
+          <Suspense fallback={<h1>My Data Loading.......</h1>}>
+            {/* Data feteching function calling  2 times from Home and Contact component but for react-query its fetches only 1 time. */}
             <Home />
             <Contact />
           </Suspense>
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,52 @@ import React from "react";
 import { useQuery } from "react-query";
 import FetchingData from "./FetchingData";
 
+
 function Home() {
   const { data } = useQuery("mykey", () => FetchingData());
-  const x = { ...data.data.products };
-  console.log("fuck-2", x);
+  const x = data.data.products;
 
-  return <div className="w-96 bg-zinc-400 h-72"></div>;
+  return (
+    <div className="w-full">
+      <div className="flex justify-around p-2 mx-2 my-1 bg-white">
+        <div className="w-1/4 p-2 text-sm tracking-wide text-left"> title </div>
+
+        <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+          description
+        </div>
+        <div className="w-1/4 p-2 text-sm tracking-wide text-left">brand</div>
+        <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+          category
+        </div>
+      </div>
+      {x && x.map((item) => (
+        <div className="flex justify-around p-2 mx-2 my-1 ">
+          <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+            {" "}
+            {item.title}
+          </div>
+
+          <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+            {item.description}
+          </div>
+          <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+            {item.brand}
+          </div>
+          <div className="w-1/4 p-2 text-sm tracking-wide text-left">
+            {item.category}
+          </div>
+        </div>
+      )
+      )
+    }
+    </div>
+  );
 }
 
-export default Home;
+export default Home; 
+
+// Object.keys(x).map((itemkey) => (
+//   <li>
+//     {itemkey}:{x[itemkey]["brand"]}
+//   </li>
+// ))
